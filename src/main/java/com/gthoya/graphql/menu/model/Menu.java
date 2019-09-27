721f44d06dcb188a48925738410b8576f0e7d1a5
jacob <jacob.gt@kakaocorp.com>
2019-09-27 18:28:39 +0900

    menu 일단 기본 동작 되도록 수정

--- src/main/java/com/gthoya/graphql/menu/model/Menu.java
+++ src/main/java/com/gthoya/graphql/menu/model/Menu.java
@@ -6,13 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.List;
 
 @Data
 @Entity
@@ -26,13 +26,13 @@ public class Menu {
     @Id
     private Long coffeeId;
 
-    @ManyToMany
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cafeId", nullable = false, insertable = false, updatable = false)
-    private List<Cafe> cafe;
+    private Cafe cafe;
 
-    @ManyToMany
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "coffeeId", nullable = false, insertable = false, updatable = false)
-    private List<Coffee> coffee;
+    private Coffee coffee;
 }
 
 class MenuId implements Serializable {
